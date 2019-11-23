1aa7a1eb775ef0aa73f96a82388da524bec8d5a2
NKM6795 <bodyaufml@gmail.com>
2019-11-23 07:02:56 -0800

    Fixed FixnumLock interface, added lock, unlock, getId, register and unregister to BakeryLock

--- Lab/Lamport_algorithm/BakeryLock.h
+++ Lab/Lamport_algorithm/BakeryLock.h
@@ -2,48 +2,143 @@
 
 
 #include "FixnumLock.h"
+#include <array>
+#include <algorithm> 
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <numeric>
+#include <assert.h>
 
 
-class BakeryLock : public FixnumLock
+namespace bakery_lock
 {
-	void lock() override
+	template<size_t MaxThreadsCount>
+	class BakeryLock : public FixnumLock
 	{
+	public:
+		void lock() override
+		{
+			size_t id = getId();
+			assert(id != std::numeric_limits<size_t>::max());
 
-	}
+			is_enter_array[id] = true;
 
-	void lockInterruptibly() override
-	{
+			size_t max_item = number_array[0];
+			for (size_t i = 0; i < MaxThreadsCount; ++i)
+			{
+				max_item = std::max(max_item, number_array[i]);
+			}
+			number_array[id] = max_item;
 
-	}
+			is_enter_array[id] = false;
 
-	void unlock() override
-	{
+			for (size_t i = 0; i < MaxThreadsCount; ++i)
+			{
+				while (is_enter_array[i])
+				{
+					std::this_thread::yield();
+				}
 
-	}
+				while ((number_array[i] != 0) 
+					&& (number_array[id] > number_array[i] || (number_array[id] == number_array[i] && id > i)))
+				{
+					std::this_thread::yield();
+				}
+			}
+		}
 
-	bool tryLock() override
-	{
-		return false;
-	}
+		void unlock() override
+		{
+			size_t id = getId();
+			assert(id != std::numeric_limits<size_t>::max());
 
-	bool tryLock(size_t timeout) override
-	{
-		return false;
-	}
+			number_array[id] = 0;
+		}
+
+		bool tryLock() override
+		{
+			size_t id = getId();
+			assert(id != std::numeric_limits<size_t>::max());
 
 
-	size_t getId() override
-	{
-		return 0;
-	}
 
-	void register_thread() override
-	{
+			return false;
+		}
 
-	}
+		bool tryLock(size_t timeout_in_milliseconds) override
+		{
+			size_t id = getId();
+			assert(id != std::numeric_limits<size_t>::max());
 
-	void unregister_thread() override
-	{
 
-	}
-};
+
+			return false;
+		}
+
+
+		size_t getId() override
+		{
+			std::lock_guard<std::mutex> lock(thread_id_to_number_lock);
+
+			std::thread::id id = std::this_thread::get_id();
+			for (size_t i = 0; i < thread_id_to_number_array.size(); ++i)
+			{
+				if (thread_id_to_number_array[i].first == id)
+				{
+					return thread_id_to_number_array[i].second;
+				}
+			}
+
+			return std::numeric_limits<size_t>::max();
+		}
+
+		void register_thread() override
+		{
+			register_thread(std::this_thread::get_id());
+		}
+
+		void register_thread(std::thread::id id) override
+		{
+			std::lock_guard<std::mutex> lock(thread_id_to_number_lock);
+
+			for (size_t i = 0; i < thread_id_to_number_array.size(); ++i)
+			{
+				if (thread_id_to_number_array[i].first == std::thread::id{})
+				{
+					thread_id_to_number_array[i].first = id;
+					thread_id_to_number_array[i].second = i;
+					return;
+				}
+			}
+		}
+
+		void unregister_thread() override
+		{
+			unregister_thread(std::this_thread::get_id());
+		}
+
+
+		void unregister_thread(std::thread::id id) override
+		{
+			std::lock_guard<std::mutex> lock(thread_id_to_number_lock);
+
+			for (size_t i = 0; i < thread_id_to_number_array.size(); ++i)
+			{
+				if (thread_id_to_number_array[i].first == id)
+				{
+					thread_id_to_number_array[i].first = std::thread::id{};
+					thread_id_to_number_array[i].second = 0;
+					return;
+				}
+			}
+		}
+
+	private:
+		std::array<bool, MaxThreadsCount> is_enter_array{ false };
+		std::array<size_t, MaxThreadsCount> number_array{ 0 };
+
+		std::array<std::pair<std::thread::id, size_t>, MaxThreadsCount> thread_id_to_number_array{};
+		std::mutex thread_id_to_number_lock{};
+	};
+}
--- Lab/Lamport_algorithm/FixnumLock.h
+++ Lab/Lamport_algorithm/FixnumLock.h
@@ -1,14 +1,25 @@
 #pragma once
 
+
+#include <thread>
+
+
 class FixnumLock
 {
+public:
+	FixnumLock() {}
+
+	FixnumLock(FixnumLock&&) = delete;
+	FixnumLock& operator=(FixnumLock&&) = delete;
+
 	virtual void lock() = 0;
-	virtual void lockInterruptibly() = 0;
 	virtual void unlock() = 0;
 	virtual bool tryLock() = 0;
-	virtual bool tryLock(size_t timeout) = 0;
+	virtual bool tryLock(size_t timeout_in_milliseconds) = 0;
 
 	virtual size_t getId() = 0;
 	virtual void register_thread() = 0;
+	virtual void register_thread(std::thread::id id) = 0;
 	virtual void unregister_thread() = 0;
+	virtual void unregister_thread(std::thread::id id) = 0;
 };
