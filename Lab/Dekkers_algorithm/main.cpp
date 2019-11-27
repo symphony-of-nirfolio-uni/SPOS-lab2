f9e1166e88eebf829c7371f09e195aead279fc22
SymphonyOfTranquility <mysechko01@gmail.com>
2019-11-27 09:57:10 +0200

    add check on race condition

--- Lab/Dekkers_algorithm/main.cpp
+++ Lab/Dekkers_algorithm/main.cpp
@@ -3,19 +3,7 @@
 #include <iostream>
 #include "Algorithm.h"
 
-using std::cout;
-
 int main()
 {
-	int step = 1000000;
-	int answer = create_race_condition(step);
-	cout << "Race condition\n";
-	cout << "Value that has to be: " << step * 2 << '\n';
-	cout << "Real value: " << answer << '\n';
-
-	answer = dekkers_algorithm(step);
-	cout << "\nDekker's algorithm\n";
-	cout << "Value that has to be: " << step * 2 << '\n';
-	cout << "Real value: " << answer << '\n';
-
+	test();
 }
