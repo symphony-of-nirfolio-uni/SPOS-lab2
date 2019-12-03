ffe5c85905996279aeb7480bb4b42d6c7e8f0982
NKM6795 <bodyaufml@gmail.com>
2019-12-03 23:48:53 +0200

    Added test with 5 threads

--- Lab/Lamport_algorithm/Test.cpp
+++ Lab/Lamport_algorithm/Test.cpp
@@ -4,17 +4,17 @@
 #include <iostream>
 
 
-size_t const ThreadsCount = 4;
-size_t const TestCount = 10000000;
+size_t const ThreadsCount = 5;
+size_t const TestCount = 1000'000;
 
 
 bakery_lock::BakeryLock<ThreadsCount> bakeryLock;
 
 long tested_value = 0;
-std::array<size_t, ThreadsCount> currentNumbers{ 0 };
+std::array<size_t, ThreadsCount> currentNumbers{};
 
 
-std::chrono::milliseconds timeout{ 10 };
+std::chrono::milliseconds timeout{ 100 };
 
 void test_function(long number, size_t id)
 {
@@ -37,14 +37,13 @@ void inputTest()
 {
 	while (true)
 	{
-		long tested_value_to_output = tested_value;
-		size_t currentNumbers_1 = currentNumbers[0];
-		size_t currentNumbers_2 = currentNumbers[1];
-		size_t currentNumbers_3 = currentNumbers[2];
-		size_t currentNumbers_4 = currentNumbers[3];
-
-		std::cout << tested_value_to_output << " counters: " << currentNumbers_1 << ' ' << currentNumbers_2 << ' ' << currentNumbers_3 << ' ' << currentNumbers_4 << '\n';
-
+		std::cout << tested_value << " counters: ";
+		for (size_t i = 0; i < ThreadsCount; ++i)
+		{
+			std::cout << currentNumbers[i] << ' ';
+		}
+		std::cout << '\n';
+		
 		if (currentNumbers[0] == TestCount - 1)
 		{
 			return;
@@ -56,9 +55,9 @@ void inputTest()
 
 void test()
 {
-	std::array<long, ThreadsCount> parameters{-3, 0, 1, 2};
+	std::array<long, ThreadsCount> parameters{ -6, 0, 1, 2, 3, };
 
-	std::cout << "Numbers: -3    0    1    2\n";
+	std::cout << "Numbers: -6    0    1    2    3\n";
 
 	std::random_shuffle(parameters.begin(), parameters.end());
 
